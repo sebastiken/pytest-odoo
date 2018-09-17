5c7f153f3d36d96e7cfe54a4a7af5b0767ea44fb
Sebastian Kennedy <skennedy@e-mips.com.ar>
2018-09-17 10:21:10 -0300

    [ADD] env fixture to use in odoo tests script
    So we can write odoo test directly using pytest

--- pytest_odoo.py
+++ pytest_odoo.py
@@ -105,6 +105,13 @@ def pytest_pycollect_makemodule(path, parent):
     else:
        return OdooTestModule(path, parent)
 
+@pytest.fixture
+def env():
+    dbname = odoo.tests.common.get_db_name()
+    registry = odoo.modules.registry.RegistryManager.get(dbname)
+    cr = registry.cursor()
+    uid = odoo.SUPERUSER_ID
+    return odoo.api.Environment(cr, uid, {})
 
 # Original code of xmo-odoo:
 # https://github.com/odoo-dev/odoo/commit/95a131b7f4eebc6e2c623f936283153d62f9e70f
